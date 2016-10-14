8e2a18f88b2c565cd27d8c7babe3cda636f33476
JackBrasesco <jacbras@nuevaschool.org>
2016-10-14 09:29:06 -0700

    Classadoo autocommit

--- randy.js
+++ randy.js
@@ -2,14 +2,25 @@
 //  = dice[Math.floor(Math.random()*dice.length)];
 //gameOutput.html(response);      
 var p1guess = $("#p1guess")
+var p2guess = $("#p2guess")
+
 
 p1guess.keydown(function(enter) {
   if (enter.keyCode == 13){
-    var m
-  
+    var number1 = p1guess.val()
   p1guess.val("")
+  console.log(number1)
   }
 })
+
+p2guess.keydown(function(enter) {
+  if (enter.keyCode == 13){
+    var number2 = p2guess.val()
+    p1=2guess.val("")
+    console.log(number2)
+  }
+})
+
 
 var dice = ["1","2","3","4","5","6"]
 
